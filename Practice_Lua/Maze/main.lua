f09dc39afb0c7df4b07519fb016521460542023a
Askirbst <tsbriks@gmail.com>
2024-04-19 14:19:55 -0400

    Updated Maze
    The maze now has proper borders (no enter or exit yet) and some randomized walls on the inside.

--- Practice_Lua/Maze/main.lua
+++ Practice_Lua/Maze/main.lua
@@ -1,45 +1,86 @@
 ---@diagnostic disable: undefined-global
 local love = require("love")
 
+
 function love.load()
     local seed = os.time()
     math.randomseed(seed)
-
 end
 
 local lines = {} -- Will hold the individual lines to be drawn in love.draw()
-local line = {
 
-    posStart = {}, -- Holds the values (x, y) for the vertices of a given line
-    posStop = {}
-
-}
+local function createLine(x1, y1, x2, y2)
+    local line = {
+        posStart = {x1, y1}, -- Holds the values (x, y) for the vertices of a given line
+        posStop = {x2, y2}
+    }
+    table.insert(lines, line)
+end
 
 function createMaze()
 
     local mazeSize = 10 -- Represents the size of the maze (What size grid, i.e. 10 x 10, 5 x 5)
-    local len = 10 -- Represents the length of the lines used to create the maze
+    local len = 50 -- Represents the length of the lines used to create the maze
     local offset = 10 -- Position of top left corner of maze to the edge of the screen. Effectively the zero position of the maze
+
+    local x1, y1, x2, y2 = 0, 0, 0, 0
 
     for i = 0, mazeSize - 1, 1 do
         for j = 0, mazeSize - 1, 1 do
 
-            if i == 0 then -- Represents the first vertice in the top left corner of maze
-                line.posStart = {offset + (i * len), offset}
-                line.posStop = {offset + len + (i * len), offset}
+            -- for random number generation 0 is a horizontal line and 1 is a vertical line
+
+            if i == 0 and j <= mazeSize - 1 then -- Represents the top border of maze
+                x1, y1 = offset + (j * len), offset
+                x2, y2 = offset + len + (j * len), offset   
+                createLine(x1, y1, x2, y2)
+
+                if math.random(0, 1) == 1 then -- Adds a random vertical line from the top border
+                    x1, y1 = offset + len + (len * j), offset
+                    x2, y2 = offset + len + (len * j), offset + len
+                    createLine(x1, y1, x2, y2)
+                end
 
             end
-            if i == 0 and j == mazeSize - 1 then -- Represents the right side of the maze
-                line.posStart = {mazeSize, len}
-                line.posStop = {mazeSize, len + (i * len)}
-            end
-            if i >= 0 and j == 0 then -- Represents the left side of the maze
-                line.posStart = {offset, offset + (i * len)}
-                line.posStop = {offset, offset + len + (i * len)}
+
+            if i <= mazeSize - 1 and j == 0 then -- Represents the left border of the maze
+                x1, y1 = offset, offset + (i * len)
+                x2, y2 = offset, offset + len + (i * len)
+                createLine(x1, y1, x2, y2)
+
+                if math.random(0, 1) == 0 then -- Adds a random horizontal line from the left border
+                    x1, y1 = offset, offset + (i * len)
+                    x2, y2 = offset + len, offset + (i * len)
+                    createLine(x1, y1, x2, y2)
+                end
             end
 
-            table.insert(lines, line)
+            if i <= mazeSize - 1 and j == mazeSize - 1 then -- Represents the right border of the maze
+                x1, y1 = offset + mazeSize * len, offset + (i * len)
+                x2, y2 = offset + mazeSize * len, offset + len + (i * len)
+                createLine(x1, y1, x2, y2)
+            end
 
+            if i == mazeSize - 1 and j <= mazeSize - 1 then -- Represents the bottom border of the maze
+                x1, y1 = offset + (j * len), offset + (mazeSize * len)
+                x2, y2 = offset + len + (j * len), offset + (mazeSize * len)
+                createLine(x1, y1, x2, y2)
+            end
+
+            if i + 1 < mazeSize and j + 1 < mazeSize then -- Randomizes lines inside the border to create a maze-like picture
+
+                if math.random(0, 1) == 0 then
+                    x1, y1 = offset + (len * j), offset + (i * len)
+                    x2, y2 = offset + len + (len * j), offset + (i * len)
+                    createLine(x1, y1, x2, y2)
+                end
+
+                if math.random(0, 1) == 1 then
+                    x1, y1 = offset + len + (len * j), offset + (i * len)
+                    x2, y2 = offset + len + (len * j), offset + len + (i * len)
+                    createLine(x1, y1, x2, y2)
+                end
+            end
         end
     end
 end
@@ -51,6 +92,7 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setLineWidth(5)
     for _, line in ipairs(lines) do
         love.graphics.line(line.posStart[1], line.posStart[2], line.posStop[1], line.posStop[2])
     end
